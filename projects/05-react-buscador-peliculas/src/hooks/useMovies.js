cd767a427f86f32433a56d9a20c4ecba8658ac0b
rociobll <rocio.ballo27@gmail.com>
2026-02-20 14:32:27 +0100

    Proyecto 5 - Evitar que haga la misma busqueda 2 veces seguidas con useRef

--- projects/05-react-buscador-peliculas/src/hooks/useMovies.js
+++ projects/05-react-buscador-peliculas/src/hooks/useMovies.js
@@ -1,19 +1,22 @@
 //import withResults from '../mocks/with-results.json'
 import { searchMovies } from "../services/movies.js"
 //import withoutResults from '../mocks/no-results.json'
-import { useState } from "react"
+import { useState, useRef } from "react"
 
 export function useMovies({ search }) {
   //const [responseMovies, setResponseMovies] = useState([])
   const [movies, setMovies] = useState([])
   const [error, setError] = useState(null)
   const [loading, setLoading] = useState(false)
+  const previousSearch =useRef(search)  // para guardar la busqueda anterior sin renderizar
 
   // si no es string vacio el seaarch, hace la busqueda, si no mostramos que no hay resultados
   const getMovies = async () => {
+    if (search === previousSearch.current) return  // si es la misma busqueda no hacemos nada
     try {
       setLoading(true)
       setError(null)
+      previousSearch.current = search 
       const newMovies = await searchMovies({ search })
       setMovies(newMovies)
     } catch (e) {
