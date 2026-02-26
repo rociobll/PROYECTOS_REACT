29d3e596aa65b0d436f74fc7b3d775e57de34b4a
rociobll <rocio.ballo27@gmail.com>
2026-02-26 16:05:16 +0100

    Proyecto 7 - Midu ROUTER - Fin -  Custom hook en Search para obtener lo query params

--- projects/07-midu-router/src/pages/Search.jsx
+++ projects/07-midu-router/src/pages/Search.jsx
@@ -1,6 +1,17 @@
 import { useEffect } from "react"
 
+// custom hook que te saca los query params - no sirece para Servre as rendering
+function useQueryParams () {
+    const search = window.location.search
+    const params = new URLSearchParams(search)
+    return Object.fromEntries(params.entries())
+}
+
 export default function SearchPage ( {routeParams }) {
+    const { limit } = useQueryParams()
+
+    console.log(limit)
+
 useEffect(()=> {
     document.title =`Has buscado ${routeParams.query}`
 // eslint-disable-next-line react-hooks/exhaustive-deps
